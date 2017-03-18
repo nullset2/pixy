8406cbc660da6e02e840c0d905695c73afafd6f7
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-03-17 20:50:41 -0600

    add image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   validates :user_id, presence: true
+  validate :title, presence: true
 
   belongs_to :user
   has_many :comments
+
+  has_attached_file :image, styles: { medium: "600x600>", thumbnail: "100x100>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318021408) do
+ActiveRecord::Schema.define(version: 20170318023239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,13 @@ ActiveRecord::Schema.define(version: 20170318021408) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "title"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
