850d774191ce355136efa1acaaa20cde3aa72d3d
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-03-17 22:08:08 -0600

    adds image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,6 @@ class Post < ApplicationRecord
   has_many :comments
 
   has_attached_file :image, styles: { medium: "600x600>", thumbnail: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
