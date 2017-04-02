7c004a7e95d7a933c6b7cffa8d0d5f009f4bcfa3
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-04-02 18:37:42 -0500

    removes useless shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :comments
-  resources :posts
+  resources :comments, except: [:show, :index]
+  resources :posts, except: [:show]
   root to: "posts#index"
 end
