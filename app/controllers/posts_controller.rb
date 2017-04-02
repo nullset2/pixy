2b4698b32b3a8ec284d4578394b9a8e737ddea49
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-04-02 18:29:09 -0500

    can manage comments per user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 10)
+    @posts = Post.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
 
   # GET /posts/1
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-    def current_user_owns_the_post?(post)
-        current_user == post.user
+    def current_user_owns_this(object)
+        current_user == object.user
     end
 end
