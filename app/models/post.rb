bf806554f7f6d2cf0ae142d7a0231a46030f5c63
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-04-03 12:41:40 -0500

    better images

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  has_attached_file :image, styles: { medium: "600x600>", thumbnail: "100x100>" }
+  has_attached_file :image, styles: { medium: "900x900>", thumbnail: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
