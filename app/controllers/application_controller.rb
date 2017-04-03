ee815eb8b0b0321e88f7326bd8ca8b96f3e793eb
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-04-03 12:38:24 -0500

    workaround for search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_dummy_search, if: :devise_controller?
 
   protected
+
+  def set_dummy_search
+    @q = Post.ransack(params[:q])
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :email, :name, :password, :password_confirmation, :current_password])
