cf2b97e75130208a63e8c79d111956676e1ae80a
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-04-03 12:52:56 -0500

    moar fixezz

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  has_attached_file :image, styles: { medium: "900x900>", thumbnail: "100x100>" }
+  has_attached_file :image, styles: { medium: "900x900>", thumbnail: "100x100>" }, preserve_files: "false"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
