4cd36b215887e1f7d176603ad013783b670863d0
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2017-04-14 16:36:05 -0500

    adds mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.smtp_settings = { address: 'smtp.gmail.com', port: 587,
+              domain: 'gmail.com',
+              user_name: ENV['GMAIL_USER'],
+              password: ENV['GMAIL_PASSWORD'],
+              authentication: :plain,
+              enable_starttls_auto: true }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "pixy-rails.herokuapp.com" }
+  config.action_mailer.perform_deliveries = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
