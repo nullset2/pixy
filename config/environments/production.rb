122dbf9e1fdf16aaec5b10e54443488a19fe6a45
Alfredo <jalfredo.gallegos@gmail.com>
2025-03-26 22:31:15 -0700

    configures paperclip for minio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,23 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = { storage: :s3, s3_credentials: { bucket: ENV["S3_BUCKET_NAME"], access_key_id: ENV["AWS_ACCESS_KEY_ID"], secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], s3_region: ENV["AWS_REGION"] }, s3_host_name: ENV["AWS_ENDPOINT_URL"], use_timestamp: false }
+  s3_host = ENV['AWS_ENDPOINT']
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_protocol: 'http',
+    s3_permissions: 'private',
+    s3_region: ENV['S3_REGION'] || 'us-west-1',
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    s3_host_name: s3_host,
+    s3_options: {
+      endpoint: "http://#{s3_host}",
+      force_path_style: true
+    }
+  }
+
 
 end
