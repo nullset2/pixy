801facfd105725980ed4ca2e7ee7f73e9193aa2b
Alfredo <jalfredo.gallegos@gmail.com>
2025-03-26 18:08:18 -0700

    fixes env var for access key ID and adds env var for AWS endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = { storage: :s3, s3_credentials: { bucket: ENV["S3_BUCKET_NAME"], access_key_id: ENV["AWS_ACCESS_KEY"], secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], s3_region: ENV["AWS_REGION"] }, s3_host_name: "s3-us-west-2.amazonaws.com", use_timestamp: false }
+  config.paperclip_defaults = { storage: :s3, s3_credentials: { bucket: ENV["S3_BUCKET_NAME"], access_key_id: ENV["AWS_ACCESS_KEY_ID"], secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], s3_region: ENV["AWS_REGION"] }, s3_host_name: ENV["AWS_ENDPOINT_URL"], use_timestamp: false }
 
 end
